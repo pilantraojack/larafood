6f9f3ddaa01e0e42eb103624f18efa80a274fdfa
Daniel Taccola <daniel.taccola@011brasil.local>
2020-11-26 20:10:54 -0300

    erro ao criar client pela api

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,8 @@ Route::group([
 ], function() {
     Route::get('/auth/me', 'Api\Auth\AuthClientController@me');
     Route::post('/auth/logout', 'Api\Auth\AuthClientController@logout');
+
+    Route::post('/auth/v1/orders', 'Api\OrderApiController@store');
 });
 
 // Api v1 group
