678828844e957d7fb14954c7e774f43baabe091d
Daniel Taccola <daniel.taccola@011brasil.local>
2020-12-11 12:35:13 -0300

    blade home

--- resources/views/admin/pages/home/home.blade.php
+++ resources/views/admin/pages/home/home.blade.php
@@ -13,111 +13,94 @@
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-users"></i>
                 </span>
-
                 <div class="info-box-content">
                     <span class="info-box-text">Usuários</span>
                     <span class="info-box-number">{{ $totalUsers }}</span>
                 </div>
             </div>
         </div>
-
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-tablet"></i>
                 </span>
-
                 <div class="info-box-content">
                     <span class="info-box-text">Mesas</span>
                     <span class="info-box-number">{{ $totalTables }}</span>
                 </div>
             </div>
         </div>
-
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-layer-group"></i>
                 </span>
-
                 <div class="info-box-content">
                     <span class="info-box-text">Categorias</span>
                     <span class="info-box-number">{{ $totalCategories }}</span>
                 </div>
             </div>
         </div>
-
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-briefcase"></i>
                 </span>
-
                 <div class="info-box-content">
                     <span class="info-box-text">Produtos</span>
                     <span class="info-box-number">{{ $totalProducts }}</span>
                 </div>
             </div>
         </div>
-
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-building"></i>
                 </span>
-
                 <div class="info-box-content">
                     <span class="info-box-text">Empresas</span>
                     <span class="info-box-number">{{ $totalTenants }}</span>
                 </div>
             </div>
         </div>
-
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-list-alt"></i>
                 </span>
-
                 <div class="info-box-content">
                     <span class="info-box-text">Planos</span>
                     <span class="info-box-number">{{ $totalPlans }}</span>
                 </div>
             </div>
         </div>
-
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-address-card"></i>
                 </span>
-
                 <div class="info-box-content">
-                    <span class="info-box-text">Roles</span>
+                    <span class="info-box-text">Cargos</span>
                     <span class="info-box-number">{{ $totalRoles }}</span>
                 </div>
             </div>
         </div>
-
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-id-badge"></i>
                 </span>
-
                 <div class="info-box-content">
                     <span class="info-box-text">Perfis</span>
                     <span class="info-box-number">{{ $totalProfiles }}</span>
                 </div>
             </div>
         </div>
-
         <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="info-box">
                 <span class="info-box-icon bg-aqua">
                     <i class="fas fa-lock"></i>
                 </span>
-
                 <div class="info-box-content">
                     <span class="info-box-text">Permissões</span>
                     <span class="info-box-number">{{ $totalPermissions }}</span>
