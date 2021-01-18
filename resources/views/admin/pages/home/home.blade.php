eba0cf5be81efb01c8c7ff81b88df1f79d06b942
Daniel Taccola <daniel.taccola@011brasil.local>
2021-01-18 18:28:25 -0300

    diretizes @can na home blade

--- resources/views/admin/pages/home/home.blade.php
+++ resources/views/admin/pages/home/home.blade.php
@@ -7,105 +7,124 @@
 @stop
 
 @section('content')
+
     <div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon">
-                    <a class="text-dark" href="{{ route('users.index') }}"><i class="fas fa-users"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('users.index') }}">Usuários</a></span>
-                    <span class="info-box-number">{{ $totalUsers }}</span>
+        @can('users')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon">
+                        <a class="text-dark" href="{{ route('users.index') }}"><i class="fas fa-users"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('users.index') }}">Usuários</a></span>
+                        <span class="info-box-number">{{ $totalUsers }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua">
-                    <a class="text-dark" href="{{ route('tables.index') }}"><i class="fas fa-tablet"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('tables.index') }}">Mesas</a></span>
-                    <span class="info-box-number">{{ $totalTables }}</span>
+        @endcan
+        @can('tables')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua">
+                        <a class="text-dark" href="{{ route('tables.index') }}"><i class="fas fa-tablet"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('tables.index') }}">Mesas</a></span>
+                        <span class="info-box-number">{{ $totalTables }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua">
-                    <a class="text-dark" href="{{ route('categories.index') }}"><i class="fas fa-layer-group"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('categories.index') }}">Categorias</a></span>
-                    <span class="info-box-number">{{ $totalCategories }}</span>
+        @endcan
+        @can('categories')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua">
+                        <a class="text-dark" href="{{ route('categories.index') }}"><i class="fas fa-layer-group"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('categories.index') }}">Categorias</a></span>
+                        <span class="info-box-number">{{ $totalCategories }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua">
-                    <a class="text-dark" href="{{ route('products.index') }}"><i class="fas fa-briefcase"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('products.index') }}">Produtos</a></span>
-                    <span class="info-box-number">{{ $totalProducts }}</span>
+        @endcan
+        @can('products')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua">
+                        <a class="text-dark" href="{{ route('products.index') }}"><i class="fas fa-briefcase"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('products.index') }}">Produtos</a></span>
+                        <span class="info-box-number">{{ $totalProducts }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua">
-                    <a class="text-dark" href="{{ route('tenants.index') }}"><i class="fas fa-building"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('tenants.index') }}">Empresas</a></span>
-                    <span class="info-box-number">{{ $totalTenants }}</span>
+        @endcan
+        @can('tenants')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua">
+                        <a class="text-dark" href="{{ route('tenants.index') }}"><i class="fas fa-building"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('tenants.index') }}">Empresas</a></span>
+                        <span class="info-box-number">{{ $totalTenants }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua">
-                    <a class="text-dark" href="{{ route('plans.index') }}"><i class="fas fa-list-alt"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('plans.index') }}">Planos</a></span>
-                    <span class="info-box-number">{{ $totalPlans }}</span>
+        @endcan
+        @can('plans')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua">
+                        <a class="text-dark" href="{{ route('plans.index') }}"><i class="fas fa-list-alt"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('plans.index') }}">Planos</a></span>
+                        <span class="info-box-number">{{ $totalPlans }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua">
-                    <a class="text-dark" href="{{ route('roles.index') }}"><i class="fas fa-address-card"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('roles.index') }}">Cargos</a></span>
-                    <span class="info-box-number">{{ $totalRoles }}</span>
+        @endcan
+        @can('roles')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua">
+                        <a class="text-dark" href="{{ route('roles.index') }}"><i class="fas fa-address-card"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('roles.index') }}">Cargos</a></span>
+                        <span class="info-box-number">{{ $totalRoles }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua">
-                    <a class="text-dark" href="{{ route('profiles.index') }}"><i class="fas fa-id-badge"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('profiles.index') }}">Perfis</a></span>
-                    <span class="info-box-number">{{ $totalProfiles }}</span>
+        @endcan
+        @can('profiles')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua">
+                        <a class="text-dark" href="{{ route('profiles.index') }}"><i class="fas fa-id-badge"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('profiles.index') }}">Perfis</a></span>
+                        <span class="info-box-number">{{ $totalProfiles }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua">
-                    <a class="text-dark" href="{{ route('permissions.index') }}"><i class="fas fa-lock"></i></a>
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('permissions.index') }}">Permissões</a></span>
-                    <span class="info-box-number">{{ $totalPermissions }}</span>
+        @endcan
+        @can('permissions')
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua">
+                        <a class="text-dark" href="{{ route('permissions.index') }}"><i class="fas fa-lock"></i></a>
+                    </span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><a class="text-dark font-weight-bold" href="{{ route('permissions.index') }}">Permissões</a></span>
+                        <span class="info-box-number">{{ $totalPermissions }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endcan
     </div>
 @stop
