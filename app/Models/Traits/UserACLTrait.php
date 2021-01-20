f203ad838b5d2228af28384ad1a8a9f6049b3052
Daniel Taccola <daniel.taccola@011brasil.local>
2021-01-20 15:29:06 -0300

    roles e permissions funcionando

--- app/Models/Traits/UserACLTrait.php
+++ app/Models/Traits/UserACLTrait.php
@@ -6,67 +6,51 @@ use App\Models\Tenant;
 
 trait UserACLTrait
 {
-    public function permissions()
+    public function permissions(): array
     {
-        // $permissionsPlan = $this->permissionsPlan();
-        // $permissionsRole = $this->permissionsRole();
-
-        // $permissions = [];
-        // foreach ($permissionsRole as $permission) {
-        //     if (in_array($permission, $permissionsPlan))
-        //         array_push($permissions, $permission);
-        // }
-
-        // return $permissions;
-        $tenant = $this->tenant()->first();
-        $plan = $tenant->plan;
+        $permissionsPlan = $this->permissionsPlan();
+        $permissionsRole = $this->permissionsRole();
 
         $permissions = [];
-        foreach($plan->profiles as $profile) {
-            foreach($profile->permissions as $permission) {
-                array_push($permissions, $permission->name);
-            };
+        foreach ($permissionsRole as $permission) {
+            if (in_array($permission, $permissionsPlan))
+                array_push($permissions, $permission);
         }
 
         return $permissions;
     }
 
     // método que retorna as permissoes do plano
-    public function permissionsPlan(): array {
-        // // recupera o tenant com o plano, os perfis do plano, e as permissoes do perfil
-        // $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
+    public function permissionsPlan(): array
+    {
+        // $tenant = $this->tenant;
         // $plan = $tenant->plan;
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
+        $plan = $tenant->plan;
 
-        // $permissions = [];
-        // // percorre os perfis do plano
-        // foreach($plan->profiles as $profile){
-        //     // percorre as permissoes do perfil
-        //     foreach($profile->permissions as $permission){
-        //         // preenche o array de permissions e mostra o nome das permissoes
-        //         array_push($permissions, $permission->name);
-        //     }
-        // }
-        // // retorna as permissoes
-        // return $permissions;
-    }
-    // método que retorna as permissoes do cargo
-    public function permissionsRole(): array {
-        // // recupera as permissoes de um cargo
-        // $roles = $this->roles()->with('permissions')->get();
-        // // cria um array vazio para receber as permissoes
-        // $permissions = [];
-        // // recupera as permissoes dos cargos e joga para dentro do array de permissoes
-        // foreach($roles as $role){
-        //     foreach($role->permissions as $permission) {
-        //         array_push($permissions, $permission->name);
-        //     }
-        // }
+        $permissions = [];
+        foreach ($plan->profiles as $profile) {
+            foreach ($profile->permissions as $permission) {
+                array_push($permissions, $permission->name);
+            }
+        }
 
-        // // retorna as permissoes
-        // return $permissions;
+        return $permissions;
     }
 
+    public function permissionsRole(): array
+    {
+        $roles = $this->roles()->with('permissions')->get();
 
+        $permissions = [];
+        foreach ($roles as $role) {
+            foreach ($role->permissions as $permission) {
+                array_push($permissions, $permission->name);
+            }
+        }
+
+        return $permissions;
+    }
 
     // verifica se o usuário tem permissões, retorna true or false
     public function hasPermission(string $permissionName): bool {
